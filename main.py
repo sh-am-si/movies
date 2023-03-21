cca65cb32819f084ffbf33b6cd4ab8543a9986bb
Volodya Byelobrov <volodia.byelobrov@gmail.com>
2023-03-21 02:36:14 +0100

    cast finished

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template
-from tmdb_client import get_popular_movies
+import tmdb_client
+
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/home')
 def homepage():
-    movies = get_popular_movies()
+    movies = tmdb_client.get_popular_movies(movie_num=8)
     return render_template("homepage.html", movies=movies)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+
+    details = tmdb_client.get_single_movie(movie_id)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    return render_template("movie_details.html", movie=details, cast=cast)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -20,17 +20,45 @@ poster_sizes=[
       "original"
     ]
 
-def get_popular_movies() -> dict:
+def get_popular_movies(movie_num: int = 8) -> dict:
+    assert movie_num > 1
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = token
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json;charset=utf-8"
     }
     response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return response.json()['results'][:movie_num]
 
 def get_poster_url(poster_api_path, size: str="w342") -> str:
+    # assert size in poster_sizes
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    if 'cast' in response.json():
+        return response.json()["cast"]
+    else:
+        return []
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
 
